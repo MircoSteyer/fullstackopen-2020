e670a8ff0fe3a86d5b34a9bcd764106f80db5138
unknown <mircosteyer@web.de>
2020-08-07 19:42:16 +0200

    1.7 to 1.11 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,42 +3,52 @@ import ReactDOM from "react-dom"
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const Statistic = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
 const Statistics = ({counts}) => {
     const [good, neutral, bad] = counts
+    const sumOfReviews = counts.reduce((sum, current) => sum + current)
+
+    if (sumOfReviews <= 0) return <div>No feedback given.</div>
+    const average = ((good - bad)/sumOfReviews).toFixed(2)
+    const percentageOfPositiveReviews = ((good/sumOfReviews) * 100).toFixed(2) + "%"
+
     return (
-        <div>
-            <h1>Statistics</h1>
-            <p>Good {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
-        </div>
-    );
+        <table>
+            <thead>
+                <tr>
+                    <th>Stat</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <Statistic text={"Good"} value={good}/>
+                <Statistic text={"Neutral"} value={neutral}/>
+                <Statistic text={"Bad"} value={bad}/>
+                <Statistic text={"All"} value={sumOfReviews}/>
+                <Statistic text={"Average"} value={average}/>
+                <Statistic text={"Positive"} value={percentageOfPositiveReviews}/>
+            </tbody>
+        </table>
+    )
 };
+
+
 
 const App = () => {
 
     const [goodCount, setGoodCount] = useState(0)
     const [neutralCount, setNeutralCount] = useState(0)
     const [badCount, setBadCount] = useState(0)
+    const counts = [goodCount, neutralCount, badCount]
 
-    /*    const increaseCount = (type) => {
-        const handler = () => {
-            switch(type) {
-                case "good":
-                    setGoodCount(goodCount + 1)
-                    break
-                case "neutral":
-                    setNeutralCount(neutralCount + 1)
-                    break
-                case "bad":
-                    setBadCount(badCount + 1)
-                    break
-                default:
-                    break
-            }
-        }
-        return handler
-    }*/
     const increaseCount = (type) => () => {
         switch(type) {
             case "good":
@@ -54,16 +64,14 @@ const App = () => {
                 break
         }
     }
-
-
-
     return (
         <div>
             <h1>Give Feedback</h1>
             <Button onClick={increaseCount("good")} text={"Good"}/>
             <Button onClick={increaseCount("neutral")} text={"Neutral"}/>
             <Button onClick={increaseCount("bad")} text={"Bad"}/>
-            <Statistics counts={[goodCount, neutralCount, badCount]}/>
+            <h1>Statistics</h1>
+            <Statistics counts={counts}/>
         </div>
     );
 };
